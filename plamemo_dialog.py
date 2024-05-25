b3f0400b11b98687e7dfae128b53cd53b2f8e742
toosakarin1412 <toosakarin1412@gmail.com>
2024-05-26 00:21:12 +0700

    update jump dan label working file

--- plamemo_dialog.py
+++ plamemo_dialog.py
@@ -15,6 +15,7 @@ def import_file():
     global nama_file
     file_path = filedialog.askopenfilename(title="Pilih File Scenario (txt)", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
     nama_file = os.path.basename(file_path)
+    label_file.config(text=nama_file)
     if file_path:
         with open(file_path, 'r') as file:
             global dialog
@@ -68,7 +69,6 @@ def prev_dialog():
     global dialog
     current -= 1
     if (current < 0):
-        print("bruh")
         current = len(dialog)-1
     update_text(current)
 
@@ -86,9 +86,19 @@ def save_dialog(event):
 
     dialog[current] = "\n".join(result)
 
+def jump_dialog(event):
+    global current
+    global dialog
+    current = int(jump.get())-1
+    if current > len(dialog)-1:
+        current = 0
+    update_text(current)
+    jump.delete(0, tk.END)
+
+
 root = tk.Tk()
 root.title("Plamemo Dialog")
-root.geometry("731x250")
+root.geometry("730x275")
 root.resizable(width=False, height=False)
 
 # Import Export Frame
@@ -112,21 +122,33 @@ prev_button = tk.Button(navFrame, text="<<", command=prev_dialog)
 prev_button.pack(side=tk.LEFT)
 
 # Dialog Frame
-dialogFrame = tk.Frame(root)
-dialogFrame.pack(side=tk.TOP)
+dialogFrame = tk.Frame(root, pady=10)
+dialogFrame.pack(side=tk.TOP, fill=tk.X)
 
 nameFrame = tk.Frame(dialogFrame)
-nameFrame.pack(side=tk.LEFT, fill=tk.X)
+nameFrame.pack(side=tk.TOP, fill=tk.X)
 
 label_nama = tk.Label(nameFrame, text="<...>", font=("Helvetica", 24))
 label_nama.pack(side=tk.LEFT)
 
-label_posisi = tk.Label(root, text="0/0", font=("Helvetica", 21))
-label_posisi.pack(side=tk.BOTTOM)
-
-text = tk.Text(root, height = 3, font=("Seibi-Ai-B_Regular", 22))
+text = tk.Text(dialogFrame, height = 3, font=("Seibi-Ai-B_Regular", 22))
 text.pack(side=tk.TOP)
 text.bind('<KeyRelease>', save_dialog)
 
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side=tk.TOP, fill=tk.X)
+
+label_posisi = tk.Label(bottomFrame, text="0/0", font=("Helvetica", 21))
+label_posisi.pack(side=tk.TOP)
+
+label_file = tk.Label(bottomFrame, font=("Helvetica", 16))
+label_file.pack(side=tk.RIGHT)
+
+lompat = tk.Label(bottomFrame,text="Lompat " ,font=("Helvetica", 16))
+lompat.pack(side=tk.LEFT)
+
+jump = tk.Entry(bottomFrame, font=("Helvetica", 16), width=5)
+jump.pack(side=tk.LEFT)
+jump.bind('<Return>', jump_dialog)
 # Run the Tkinter event loop
 root.mainloop()
